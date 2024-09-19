adde2d22b17a7e0cd7ebbefff6b0f9aee3e3b28f
Shivama Thakur     /IT/ICICILOMBARD/VIKH <1042257@icicilombard.com>
2024-09-19 12:20:20 +0530

    init commit

---
+++ Backend.API/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly BackendDbContext _EmpDbContext;
+
+        public EmployeesController(BackendDbContext empDbContext)
+        {
+            _EmpDbContext = empDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _EmpDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
---
+++ Backend.API/Models/Employees.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.API.Models
+{
+    public class Employees
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+        public int Phone { get; set; }
+        public int Salary { get; set; }
+        public string? Department { get; set; }
+
+    }
+}
